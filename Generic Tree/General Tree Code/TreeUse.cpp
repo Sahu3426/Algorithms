85c3a240efe0168af28709d3b976901f6bb4f833
Sanjay Kumar Sahu <33930792+Sahu3426@users.noreply.github.com>
2019-05-20 16:24:09 +0530

    Update TreeUse.cpp

--- Generic Tree/General Tree Code/TreeUse.cpp
+++ Generic Tree/General Tree Code/TreeUse.cpp
@@ -61,7 +61,25 @@ void printTree(TreeNode<int>* root) {
 	}
 }
 
+void printAtLevelK(TreeNode<int>* root, int k) {
+	if (root == NULL) {
+		return;
+	}
+
+	if (k == 0) {
+		cout << root->data << endl;
+		return;
+	}
+
+	for (int i = 0; i < root->children.size(); i++) {
+		printAtLevelK(root->children[i], k - 1);
+	}
+}
+
 int numNodes(TreeNode<int>* root) {
+	if (root == NULL) {
+		return 0;
+	}
 	int ans = 1;
 	for (int i = 0; i < root->children.size(); i++) {
 		ans += numNodes(root->children[i]);
@@ -69,6 +87,24 @@ int numNodes(TreeNode<int>* root) {
 	return ans;
 }
 
+void preorder(TreeNode<int>* root) {
+	if (root == NULL) {
+		return;
+	}
+	cout << root->data << " ";
+	for (int i = 0; i < root->children.size(); i++) {
+		preorder(root->children[i]);
+	}
+}
+
+void deleteTree(TreeNode<int>* root) {
+	for (int i = 0; i < root->children.size(); i++) {
+		deleteTree(root->children[i]);
+	}
+	delete root;
+}
+
+// 1 3 2 3 4 2 5 6 2 7 8 0 0 0 0 1 9 0
 int main() {
 	/*TreeNode<int>* root = new TreeNode<int>(1);
 	TreeNode<int>* node1 = new TreeNode<int>(2);
@@ -78,6 +114,12 @@ int main() {
 	*/
 	TreeNode<int>* root = takeInputLevelWise();
 	printTree(root);
+	cout << "num: " << numNodes(root) << endl;
+	cout << "Level 2:" << endl;
+	printAtLevelK(root, 2);
+	preorder(root);
 	// TODO delete the tree
+	delete root;
+	//deleteTree(root);
 }
 
