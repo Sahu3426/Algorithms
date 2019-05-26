afd3e0bf0fc8fabf98e8c0071d5da57d451693ef
Sanjay Kumar Sahu <33930792+Sahu3426@users.noreply.github.com>
2019-05-26 14:52:04 +0530

    Create PQ_MaxHeap.cpp

---
+++ PriorityQueue_MinHeap/PQ_MaxHeap.cpp
@@ -0,0 +1,130 @@
+#include <vector>
+
+/*******************
+ * Main function -
+*
+int main() {
+    PriorityQueue pq;
+    int choice;
+    cin >> choice;
+    while(choice != -1) {
+        switch(choice) {
+            case 1 : // insert
+                int element;
+                cin >> element;
+                pq.insert(element);
+                break;
+            case 2 : // getMin
+                cout << pq.getMin() << endl;
+                break;
+            case 3 : // removeMin
+                cout << pq.removeMin() << endl;
+                break;
+            case 4 : // size
+                cout << pq.getSize() << endl;
+                break;
+            case 5 : // isEmpty
+                if(pq.isEmpty()) {
+                    cout << "true" << endl;
+                }
+                else {
+                    cout << "false" << endl;
+                }
+            default :
+                return 0;
+        }
+        cin >> choice;
+    }
+}
+**************************/
+
+
+class PriorityQueue {
+    vector<int> pq;
+    
+    public :
+    
+    PriorityQueue() {
+        
+    }
+    
+    bool isEmpty() {
+        return pq.size() == 0;
+    }
+    
+    // Return the size of priorityQueue - no of elements present
+    int getSize() {
+        return pq.size();
+    }
+    
+    int getMax() {
+        if(isEmpty()) {
+            return 0;		// Priority Queue is empty
+        }
+        return pq[0];
+    }
+    
+    void insert(int element) {
+        pq.push_back(element);
+        
+        int childIndex = pq.size() - 1;
+        
+        while(childIndex > 0) {
+            int parentIndex = (childIndex - 1) / 2;
+            
+            if(pq[childIndex] > pq[parentIndex]) {
+                int temp = pq[childIndex];
+                pq[childIndex] = pq[parentIndex];
+                pq[parentIndex] = temp;
+            }
+            else {
+                break;
+            }
+            childIndex = parentIndex;
+        }
+        
+    }
+    
+    int removeMax() {
+        // Complete this function
+      if(pq.empty())
+        {
+            return 0;
+        }
+        int index=pq.size()-1;
+        int ans=pq[0];
+        pq[0]=pq[index];
+        pq.pop_back();
+        int parentIndex=0;
+        int childIndex1=(2*parentIndex)+1;
+        int childIndex2=(2*parentIndex)+2;
+        while(childIndex2<pq.size()-1)
+        {
+            int minindex=parentIndex;
+            if(pq[minindex]<pq[childIndex1]||pq[minindex]<pq[childIndex2]){
+            if(pq[minindex]<pq[childIndex1])
+            {
+                minindex=childIndex1;
+            }
+            if(pq[minindex]<pq[childIndex2])
+            {
+                minindex=childIndex2;
+            }
+            int temp=pq[minindex];
+            pq[minindex]=pq[parentIndex];
+            pq[parentIndex]=temp;
+            parentIndex=minindex;
+           childIndex1=(2*parentIndex)+1;
+           childIndex2=(2*parentIndex)+2;}
+           else
+           {
+               break;
+           }
+        }
+      return ans;
+    
+        
+    }
+    
+    
+};
